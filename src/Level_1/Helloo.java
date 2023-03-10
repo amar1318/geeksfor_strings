42a2d90879220fd1481ff8923b33dea65e87c8db
amar <gawadeamar3@gmail.com>
2023-03-10 08:34:28 +0530

    String Problems

--- src/Level_1/Helloo.java
+++ src/Level_1/Helloo.java
@@ -2,6 +2,6 @@ package Level_1;
 
 public class Helloo {
     public static void main(String[] args) {
-        System.out.println("hello, is it running now??");
+        System.out.println("ohh, it's running now..");
     }
 }
